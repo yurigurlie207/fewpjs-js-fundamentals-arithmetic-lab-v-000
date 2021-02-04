ccaaf6c4910c64ad0e61e9622ee716d8ef1ee40d
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-04 15:16:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 // Write your code here
 
-const var1 = 2
-const var2 = 31
-const multiply = var1 * var2
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
