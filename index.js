d22fb7a40456f13b7fdfdb1aa9f2145dd70d066c
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-04 15:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const num1 = 2
 const num2 = 31
-const multiply = var1 * var2
+const multiply = num1 * num2
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
