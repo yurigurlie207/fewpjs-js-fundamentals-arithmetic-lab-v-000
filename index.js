c5d2b0da01b371b3722cd5a78e82b0bb8bb1a5ab
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-04 15:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Write your code here
 
-const var1 = 2
-const var2 = 31
+const num1 = 2
+const num2 = 31
 const multiply = var1 * var2
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
